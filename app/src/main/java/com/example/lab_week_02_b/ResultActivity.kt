ba5ff5545df92f715ba0136484d70f849c20c7c1
cottoncloudy <foggyacc@gmail.com>
2025-09-18 19:03:59 +0700

    Commit No. 5 – Fix ResultActivity to allow resubmitting color code without app restart

--- app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
+++ app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
@@ -4,12 +4,10 @@ import android.app.Activity
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class ResultActivity : AppCompatActivity() {
     companion object {
@@ -21,26 +19,33 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-        if(intent != null){
+        if (intent != null) {
             val colorCode = intent.getStringExtra(COLOR_KEY)
 
             val backgroundScreen =
                 findViewById<ConstraintLayout>(R.id.background_screen)
             try {
                 backgroundScreen.setBackgroundColor(Color.parseColor("#$colorCode"))
-            }
-            catch (ex: IllegalArgumentException){
-                Intent().let{
-                        errorIntent ->
+            } catch (ex: IllegalArgumentException) {
+                Intent().let { errorIntent ->
                     errorIntent.putExtra(ERROR_KEY, true)
                     setResult(Activity.RESULT_OK, errorIntent)
                     finish()
                 }
             }
+
             val resultMessage =
                 findViewById<TextView>(R.id.color_code_result_message)
-            resultMessage.text = getString(R.string.color_code_result_message,
-                colorCode?.uppercase())
+            resultMessage.text = getString(
+                R.string.color_code_result_message,
+                colorCode?.uppercase()
+            )
+        }
+
+        // ✅ Tambahin listener buat tombol BACK
+        val backButton = findViewById<Button>(R.id.back_button)
+        backButton.setOnClickListener {
+            finish() // langsung balik ke MainActivity
         }
     }
 }
